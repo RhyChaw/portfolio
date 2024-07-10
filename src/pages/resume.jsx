562bb4d5ab7e7e3fe49a6be4ddf3760073eca319
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-10 05:08:54 +0300

    fixed stuff

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import '../styling/resume.css';
 import PDF from "../data/resume.pdf";
+import { Worker } from '@react-pdf-viewer/core';
+// Import the main component
+import { Viewer } from '@react-pdf-viewer/core';
+
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
 
 function Resume() {
   return (
     <div className="resume-container">
-      <div className="resume-left">
-        <h1>Here is my current resume</h1>
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <div className='resume-pdf'>
+              <Viewer fileUrl={PDF} />
+          </div>
+          </Worker>
       </div>
-      <div className="resume-right">
-        <embed src={PDF} type="application/pdf" className="resume-pdf"/>
-      </div>
-    </div>
   );
 }
 
