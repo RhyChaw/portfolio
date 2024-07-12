d3a0e3b5a766079ea7e8c48d4ebe901ebb3b4ad6
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-12 06:42:09 +0300

    fixed certifications css

--- src/pages/certifications.jsx
+++ src/pages/certifications.jsx
@@ -2,43 +2,33 @@ import React from 'react';
 import '../styling/certifications.css';
 
 const certificationsData = [
-  { id: 1, courseName: 'Introduction to Quantum Computing', certificateLink: 'https://www.udemy.com/certificate/UC-eb95898f-1e56-403c-8dab-466467fc17c5/', completionDate: '06-06-2024', issuingOrganization: 'Udemy' },
-  { id: 2, courseName: 'Advanced CSS', certificateLink: 'https://www.udemy.com/certificate/UC-46fc113c-2139-4ce9-939e-0a9a2febb0fd/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com', completionDate: '04-07-2024', issuingOrganization: 'Udemy' },
-  
+  { id: 1, image: "/images/certifications/Quantum.png", courseName: 'Introduction to Quantum Computing', certificateLink: 'https://www.udemy.com/certificate/UC-eb95898f-1e56-403c-8dab-466467fc17c5/', completionDate: '06-06-2024', issuingOrganization: 'Udemy' },
+  { id: 2, image: "/images/certifications/AdvancedCSS.png", courseName: 'Advanced CSS', certificateLink: 'https://www.udemy.com/certificate/UC-46fc113c-2139-4ce9-939e-0a9a2febb0fd/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com', completionDate: '04-07-2024', issuingOrganization: 'Udemy' },
 ];
 
 function Certifications() {
   return (
     <div className="landing-container">
-    <div className="certifications-container">
-      <h1>My Certifications</h1>
-      <table className="certifications-table">
-        <thead>
-          <tr>
-            <th>Sl No.</th>
-            <th>Course Name</th>
-            <th>Certificate Link</th>
-            <th>Completion Date</th>
-            <th>Issuing Organization</th>
-          </tr>
-        </thead>
-        <tbody>
-          {certificationsData.map((certification, index) => (
-            <tr key={certification.id}>
-              <td>{index + 1}</td>
-              <td>{certification.courseName}</td>
-              <td>
-                <a href={certification.certificateLink} target="_blank" rel="noopener noreferrer">
+      <div className="projects-container">
+        <header className="header">
+          <h1>My Certifications</h1>
+        </header>
+        <div className="projects-grid">
+          {certificationsData.map((project, index) => (
+            <div className="project-card" key={index}>
+              <img src={project.image} alt={project.courseName} className="project-image" />
+              <div className="project-info">
+                <h2>{project.courseName}</h2>
+                <p>Issued by {project.issuingOrganization}</p>
+                <p>Completion date: {project.completionDate}</p>
+                <a href={project.certificateLink} target="_blank" rel="noopener noreferrer">
                   View Certificate
                 </a>
-              </td>
-              <td>{certification.completionDate}</td>
-              <td>{certification.issuingOrganization}</td>
-            </tr>
+              </div>
+            </div>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </div>
+      </div>
     </div>
   );
 }
