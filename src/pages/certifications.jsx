e4b7565c6be0c255cf2ceb504ca2bd617795268e
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-19 21:56:04 +0300

    added certification and project buttons

--- src/pages/certifications.jsx
+++ src/pages/certifications.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styling/certifications.css';
-
+// 
 const certificationsData = [
   { id: 1, image: "/images/certifications/Quantum.png", courseName: 'Introduction to Quantum Computing', certificateLink: 'https://www.udemy.com/certificate/UC-eb95898f-1e56-403c-8dab-466467fc17c5/', completionDate: '06-06-2024', issuingOrganization: 'Udemy' },
   { id: 2, image: "/images/certifications/AdvancedCSS.png", courseName: 'Advanced CSS', certificateLink: 'https://www.udemy.com/certificate/UC-46fc113c-2139-4ce9-939e-0a9a2febb0fd/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com', completionDate: '04-07-2024', issuingOrganization: 'Udemy' },
+  { id: 3, image: "/images/certifications/MachineLearning.png", courseName: 'Maching Learning A-Z', certificateLink: 'https://www.udemy.com/certificate/UC-a2ab1162-0958-487f-af70-a1f989323ac0/', completionDate: '19-07-2024', issuingOrganization: 'Udemy' },
+
 ];
 
 function Certifications() {
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styling/projects.css';
 
 // Import your project images
@@ -18,75 +18,111 @@ const projectsData = [
     title: 'CSGPTPRO Hackathon Project',
     description: 'App built using Python, Streamlit and Arctic',
     image: CSGPTPRO,
-    link: 'https://github.com/jadechoghari/CSGPTPRO'
+    link: 'https://github.com/jadechoghari/CSGPTPRO',
+    category: 'AI',
+    backgroundColor: '#f8f9fa'
   },
   {
     title: 'DASH Project',
     description: 'NEXT JS application with JAVA Backend for Dropshippers',
     image: DASHImage,
-    link: 'https://github.com/RhyChaw/dropshiphub'
+    link: 'https://github.com/RhyChaw/dropshiphub',
+    category: 'REACT',
+    backgroundColor: '#e9ecef'
   },
   {
     title: 'WATisZine Website',
     description: 'REACT JS app with Auth, MongoDB Backend for Zine club at UW',
     image: WATisZine,
-    link: 'https://watiszine.clubs.wusa.ca/'
+    link: 'https://watiszine.clubs.wusa.ca/',
+    category: 'REACT',
+    backgroundColor: '#dee2e6'
   },
   {
     title: 'Nexter Project',
     description: 'A web application built with Advanced CSS Grid, NPM Scripting and SASS',
     image: NexterImage,
-    link: 'https://github.com/RhyChaw/Nexter-project'
+    link: 'https://github.com/RhyChaw/Nexter-project',
+    category: 'CSS',
+    backgroundColor: '#ced4da'
   },
   {
     title: 'Trillo Project',
     description: 'A web hotel application built with Advanced CSS, and SASS',
     image: trilloImage,
-    link: 'https://github.com/RhyChaw/Trillo-project'
+    link: 'https://github.com/RhyChaw/Trillo-project',
+    category: 'CSS',
+    backgroundColor: '#adb5bd'
   },
   {
     title: 'Natours Project',
     description: 'A web tourist application built with Advanced CSS, and SASS',
     image: NatoursImage,
-    link: 'https://github.com/RhyChaw/NaTours-Project'
+    link: 'https://github.com/RhyChaw/NaTours-Project',
+    category: 'CSS',
+    backgroundColor: '#6c757d'
   },
   {
     title: 'World wise project',
     description: 'REACT Project for context API, Routing with navigate, history and Auth',
     image: WorldWise,
-    link: 'https://github.com/RhyChaw/worldwise-project'
+    link: 'https://github.com/RhyChaw/worldwise-project',
+    category: 'REACT',
+    backgroundColor: '#495057'
   },
   {
     title: 'Atomic blog project',
     description: 'REACT Project for learning context API and has Dark Mode :)',
     image: AtomicBlog,
-    link: 'https://github.com/RhyChaw/atomic-blog-context-api-project'
+    link: 'https://github.com/RhyChaw/atomic-blog-context-api-project',
+    category: 'REACT',
+    backgroundColor: '#343a40'
   },
   {
     title: 'REACT Quiz',
     description: 'REACT Project for useReducer REACT Hook',
     image: ReactQuiz,
-    link: 'https://github.com/RhyChaw/React-Quiz-project'
+    link: 'https://github.com/RhyChaw/React-Quiz-project',
+    category: 'REACT',
+    backgroundColor: '#212529'
   },
   {
     title: 'Use Popcorn',
     description: 'A movie tracking REACT application using fetch APIs',
     image: UsePopcornImage,
-    link: 'https://github.com/RhyChaw/07-UsePopcorn'
+    link: 'https://github.com/RhyChaw/07-UsePopcorn',
+    category: 'REACT',
+    backgroundColor: '#0d6efd'
   },
 ];
 
+const categories = ['All', 'REACT', 'AI', 'CSS'];
+
 function Projects() {
-  const count = projectsData.length
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const filteredProjects = selectedCategory === 'All' ? projectsData : projectsData.filter(project => project.category === selectedCategory);
+
   return (
     <div className="landing-container">
       <div className="projects-container">
         <header className="header">
-          <h1>My Projects ({count})</h1>
+          <h1>My Projects ({filteredProjects.length})</h1>
         </header>
+        <div className="filter-buttons">
+            {categories.map(category => (
+              <button
+                key={category}
+                className={`filter-button ${selectedCategory === category ? 'active' : ''}`}
+                onClick={() => setSelectedCategory(category)}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
         <div className="projects-grid">
-          {projectsData.map((project, index) => (
-            <div className="project-card" key={index}>
+          {filteredProjects.map((project, index) => (
+            <div className="project-card" key={index} style={{ backgroundColor: project.backgroundColor }}>
               <img src={project.image} alt={project.title} className="project-image" />
               <div className="project-info">
                 <h2>{project.title}</h2>
@@ -104,4 +140,3 @@ function Projects() {
 }
 
 export default Projects;
-
