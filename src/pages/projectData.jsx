141dc021b51ba6e3621cb6ea54be192d867ec092
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-20 12:25:37 +0300

    added projects

---
+++ src/pages/projectData.jsx
@@ -0,0 +1,113 @@
+import trilloImage from '../images/proj/trillo.png'; 
+import NatoursImage from "../images/proj/Natours.png";
+import UsePopcornImage from "../images/proj/UsePopcorn.png";
+import DASHImage from "../images/proj/DASH.png";
+import WATisZine from "../images/proj/WATisZine.png";
+import NexterImage from "../images/proj/Nexter.png";
+import CSGPTPRO from "../images/proj/csgptpro.png";
+import ReactQuiz from "../images/proj/ReactQuiz.png";
+import WorldWise from "../images/proj/Worldwise.png";
+import AtomicBlog from "../images/proj/AtomicBlog.png";
+import G12Proj from "../images/proj/G12Proj.png"
+
+const projectsData = [
+    {
+        title: 'G12',
+        description: 'Startup App built using Vite React and Firebase',
+        image: G12Proj,
+        link: 'https://g12uni.com',
+        category: 'REACT',
+        backgroundColor: '#e9ecef'
+      },
+      {
+        title: 'JADO AI',
+        description: 'AI Chatbot which can scan through Youtube, Reddit, Wolfram ALpha, Resume scanner, People finder etc. (Addition of G12)',
+        image: G12Proj,
+        link: 'https://g12uni.com',
+        category: 'AI',
+        backgroundColor: '#f8f9fa'
+      },
+    {
+      title: 'CSGPTPRO Hackathon Project',
+      description: 'App built using Python, Streamlit and Arctic',
+      image: CSGPTPRO,
+      link: 'https://github.com/jadechoghari/CSGPTPRO',
+      category: 'AI',
+      backgroundColor: '#f8f9fa'
+    },
+    {
+      title: 'DASH Project',
+      description: 'NEXT JS application with JAVA Backend for Dropshippers',
+      image: DASHImage,
+      link: 'https://github.com/RhyChaw/dropshiphub',
+      category: 'REACT',
+      backgroundColor: '#e9ecef'
+    },
+    {
+      title: 'WATisZine Website',
+      description: 'REACT JS app with Auth, MongoDB Backend for Zine club at UW',
+      image: WATisZine,
+      link: 'https://watiszine.clubs.wusa.ca/',
+      category: 'REACT',
+      backgroundColor: '#dee2e6'
+    },
+    {
+      title: 'Nexter Project',
+      description: 'A web application built with Advanced CSS Grid, NPM Scripting and SASS',
+      image: NexterImage,
+      link: 'https://github.com/RhyChaw/Nexter-project',
+      category: 'CSS',
+      backgroundColor: '#ced4da'
+    },
+    {
+      title: 'Trillo Project',
+      description: 'A web hotel application built with Advanced CSS, and SASS',
+      image: trilloImage,
+      link: 'https://github.com/RhyChaw/Trillo-project',
+      category: 'CSS',
+      backgroundColor: '#adb5bd'
+    },
+    {
+      title: 'Natours Project',
+      description: 'A web tourist application built with Advanced CSS, and SASS',
+      image: NatoursImage,
+      link: 'https://github.com/RhyChaw/NaTours-Project',
+      category: 'CSS',
+      backgroundColor: '#6c757d'
+    },
+    {
+      title: 'World wise project',
+      description: 'REACT Project for context API, Routing with navigate, history and Auth',
+      image: WorldWise,
+      link: 'https://github.com/RhyChaw/worldwise-project',
+      category: 'REACT',
+      backgroundColor: '#495057'
+    },
+    {
+      title: 'Atomic blog project',
+      description: 'REACT Project for learning context API and has Dark Mode :)',
+      image: AtomicBlog,
+      link: 'https://github.com/RhyChaw/atomic-blog-context-api-project',
+      category: 'REACT',
+      backgroundColor: '#343a40'
+    },
+    {
+      title: 'REACT Quiz',
+      description: 'REACT Project for useReducer REACT Hook',
+      image: ReactQuiz,
+      link: 'https://github.com/RhyChaw/React-Quiz-project',
+      category: 'REACT',
+      backgroundColor: '#212529'
+    },
+    {
+      title: 'Use Popcorn',
+      description: 'A movie tracking REACT application using fetch APIs',
+      image: UsePopcornImage,
+      link: 'https://github.com/RhyChaw/07-UsePopcorn',
+      category: 'REACT',
+      backgroundColor: '#0d6efd'
+    },
+  ];
+
+export default projectsData;
+
