bc972f530a5be26891bf2fef88cce5ca2861f15b
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-21 17:57:40 +0300

    added projects

--- src/pages/projectData.jsx
+++ src/pages/projectData.jsx
@@ -12,123 +12,165 @@ import G12Proj from "../images/proj/G12Proj.png";
 import JadoAI from "../images/proj/JadoAI.png";
 import VBOman from "../images/proj/VBoman.png";
 import HestiaP from "../images/proj/HestiaP.png";
+import WorkoutTimer from "../images/proj/WorkoutTimer.png";
+import EatNSplit from "../images/proj/EatNSplit.png";
+import TravelList from "../images/proj/TravelList.png";
+import ClassyWeather from "../images/proj/ClassyWeather.png";
 
+
+
+
+const colors = {
+  css: '#ff69b4',  // Pink
+  ai: '#800080',   // Purple
+  webDev: '#333' // grey
+};
 
 const projectsData = [
-    {
-        title: 'G12',
-        description: 'Startup App built using Vite React and Firebase',
-        image: G12Proj,
-        link: 'https://g12uni.com',
-        category: 'Web Dev',
-        backgroundColor: '#e9ecef'
-      },
-      {
-        title: 'JADO AI',
-        description: 'AI Chatbot which can scan through Youtube, Reddit, Wolfram ALpha, Resume scanner, People finder etc. (Addition of G12)',
-        image: JadoAI,
-        link: 'https://g12uni.com',
-        category: 'AI',
-        backgroundColor: '#f8f9fa'
-      },
-
-    {
-      title: 'CSGPTPRO Hackathon Project',
-      description: 'App built using Python, Streamlit and Arctic',
-      image: CSGPTPRO,
-      link: 'https://github.com/jadechoghari/CSGPTPRO',
-      category: 'AI',
-      backgroundColor: '#f8f9fa'
-    },
-    {
-      title: 'VBOman Admin Panel (FreeLance)',
-      description: 'REACT JS application with Firebase Backend for Restaurant Vasanta Bhavan based in Oman',
-      image: VBOman,
-      link: 'https://vboman.com',
-      category: 'Web Dev',
-      backgroundColor: '#e9ecef'
-    },
-    {
-      title: 'Hestia | Your Next Home',
-      description: 'Student Sublet finder built using HTML, CSS, and Django(Python) For backend. Hosted using AZURE Cloud',
-      image: HestiaP,
-      link: 'https://github.com/gsaujla/HestiaProject',
-      category: 'Web Dev',
-      backgroundColor: '#e9ecef'
-    },
-    {
-      title: 'DASH Project',
-      description: 'NEXT JS application with JAVA Backend for Dropshippers',
-      image: DASHImage,
-      link: 'https://github.com/RhyChaw/dropshiphub',
-      category: 'Web Dev',
-      backgroundColor: '#e9ecef'
-    },
-    {
-      title: 'WATisZine Website',
-      description: 'REACT JS app with Auth, MongoDB Backend for Zine club at UW',
-      image: WATisZine,
-      link: 'https://watiszine.clubs.wusa.ca/',
-      category: 'Web Dev',
-      backgroundColor: '#dee2e6'
-    },
-    {
-      title: 'Nexter Project',
-      description: 'A web application built with Advanced CSS Grid, NPM Scripting and SASS',
-      image: NexterImage,
-      link: 'https://github.com/RhyChaw/Nexter-project',
-      category: 'CSS',
-      backgroundColor: '#ced4da'
-    },
-    {
-      title: 'Trillo Project',
-      description: 'A web hotel application built with Advanced CSS, and SASS',
-      image: trilloImage,
-      link: 'https://github.com/RhyChaw/Trillo-project',
-      category: 'CSS',
-      backgroundColor: '#adb5bd'
-    },
-    {
-      title: 'Natours Project',
-      description: 'A web tourist application built with Advanced CSS, and SASS',
-      image: NatoursImage,
-      link: 'https://github.com/RhyChaw/NaTours-Project',
-      category: 'CSS',
-      backgroundColor: '#6c757d'
-    },
-    {
-      title: 'World wise project',
-      description: 'REACT Project for context API, Routing with navigate, history and Auth',
-      image: WorldWise,
-      link: 'https://github.com/RhyChaw/worldwise-project',
-      category: 'Web Dev',
-      backgroundColor: '#495057'
-    },
-    {
-      title: 'Atomic blog project',
-      description: 'REACT Project for learning context API and has Dark Mode :)',
-      image: AtomicBlog,
-      link: 'https://github.com/RhyChaw/atomic-blog-context-api-project',
-      category: 'Web Dev',
-      backgroundColor: '#343a40'
-    },
-    {
-      title: 'REACT Quiz',
-      description: 'REACT Project for useReducer REACT Hook',
-      image: ReactQuiz,
-      link: 'https://github.com/RhyChaw/React-Quiz-project',
-      category: 'Web Dev',
-      backgroundColor: '#212529'
-    },
-    {
-      title: 'Use Popcorn',
-      description: 'A movie tracking REACT application using fetch APIs',
-      image: UsePopcornImage,
-      link: 'https://github.com/RhyChaw/07-UsePopcorn',
-      category: 'Web Dev',
-      backgroundColor: '#0d6efd'
-    },
-  ];
+  {
+    title: 'G12',
+    description: 'Startup App built using Vite React and Firebase',
+    image: G12Proj,
+    link: 'https://g12uni.com',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'JADO AI',
+    description: 'AI Chatbot which can scan through Youtube, Reddit, Wolfram ALpha, Resume scanner, People finder etc. (Addition of G12)',
+    image: JadoAI,
+    link: 'https://g12uni.com',
+    category: 'AI',
+    backgroundColor: colors.ai
+  },
+  {
+    title: 'CSGPTPRO Hackathon Project',
+    description: 'App built using Python, Streamlit and Arctic',
+    image: CSGPTPRO,
+    link: 'https://github.com/jadechoghari/CSGPTPRO',
+    category: 'AI',
+    backgroundColor: colors.ai
+  },
+  {
+    title: 'VBOman Admin Panel (FreeLance)',
+    description: 'REACT JS application with Firebase Backend for Restaurant Vasanta Bhavan based in Oman',
+    image: VBOman,
+    link: 'https://vboman.com',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Hestia | Your Next Home',
+    description: 'Student Sublet finder built using HTML, CSS, and Django(Python) For backend. Hosted using AZURE Cloud',
+    image: HestiaP,
+    link: 'https://github.com/gsaujla/HestiaProject',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'DASH Project',
+    description: 'NEXT JS application with JAVA Backend for Dropshippers',
+    image: DASHImage,
+    link: 'https://github.com/RhyChaw/dropshiphub',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'WATisZine Website',
+    description: 'REACT JS app with Auth, MongoDB Backend for Zine club at UW',
+    image: WATisZine,
+    link: 'https://watiszine.clubs.wusa.ca/',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Nexter Project',
+    description: 'A web application built with Advanced CSS Grid, NPM Scripting and SASS',
+    image: NexterImage,
+    link: 'https://github.com/RhyChaw/Nexter-project',
+    category: 'CSS',
+    backgroundColor: colors.css
+  },
+  {
+    title: 'Trillo Project',
+    description: 'A web hotel application built with Advanced CSS, and SASS',
+    image: trilloImage,
+    link: 'https://github.com/RhyChaw/Trillo-project',
+    category: 'CSS',
+    backgroundColor: colors.css
+  },
+  {
+    title: 'Natours Project',
+    description: 'A web tourist application built with Advanced CSS, and SASS',
+    image: NatoursImage,
+    link: 'https://github.com/RhyChaw/NaTours-Project',
+    category: 'CSS',
+    backgroundColor: colors.css
+  },
+  {
+    title: 'World wise project',
+    description: 'REACT Project for context API, Routing with navigate, history and Auth',
+    image: WorldWise,
+    link: 'https://github.com/RhyChaw/worldwise-project',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Atomic blog project',
+    description: 'REACT Project for learning context API and has Dark Mode :)',
+    image: AtomicBlog,
+    link: 'https://github.com/RhyChaw/atomic-blog-context-api-project',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Workout timer app',
+    description: 'REACT Project for practicing useMemo, useCallback and memo',
+    image: WorkoutTimer,
+    link: 'https://github.com/RhyChaw/Workout-timer-project',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'REACT Quiz',
+    description: 'REACT Project for useReducer REACT Hook',
+    image: ReactQuiz,
+    link: 'https://github.com/RhyChaw/React-Quiz-project',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Use Popcorn',
+    description: 'A movie tracking REACT application using fetch APIs',
+    image: UsePopcornImage,
+    link: 'https://github.com/RhyChaw/07-UsePopcorn',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Weather detector',
+    description: 'A Classical react app to fetch weather of different countries',
+    image: ClassyWeather,
+    link: 'https://github.com/RhyChaw/09---Weather-Detector',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Travel list',
+    description: 'A travel list application made using state in REACT',
+    image: TravelList,
+    link: 'https://github.com/RhyChaw/05---Travel-list',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+  {
+    title: 'Eat & Split',
+    description: 'A REACT APP to split bill using state',
+    image: EatNSplit,
+    link: 'https://github.com/RhyChaw/06-Eat-Split',
+    category: 'Web Dev',
+    backgroundColor: colors.webDev
+  },
+];
 
 export default projectsData;
-
